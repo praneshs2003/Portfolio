fe09b5d4107b9f5144dee8762d17e05710e4e6c4
praneshs2003 <praneshsm.cs20@bitsathy.ac.in>
2024-02-04 22:02:15 +0530

    success

---
+++ script/jscript.js
@@ -0,0 +1,33 @@
+/* java script methods:
+    1.shift() - deletes element from back in an array 
+    2.unshift() - adds elements in the array from front
+    3.push() - adds elements int the array from front
+    4.pop() - deletes elements from back in an array
+*/
+
+/*var kira={
+    nicename:'pranesh',
+    age:20,
+    education:['vidyaa vikas','bannari amman'],
+    location:{
+        state:'tamil nadu',
+        city:'coimbatore',
+        pincode:641029,
+        weather:{
+            cold:true,
+            sunny:false,
+        }
+    }
+};*/
+
+//alert("hi, this is my portfolio");
+
+var a = 10;
+function square(num)
+{
+    var ans = num*num;
+    return ans;
+}
+console.log(a);
+console.log(square(a));
+square(10);
